1cb7851e4bd59e11ea0a7acd38f0e159d9df2b08
Koowin <56014482+Koowin@users.noreply.github.com>
2022-05-23 10:06:11 +0900

    init

---
+++ src/main/java/com/valueup/backend/ValueupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.valueup.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValueupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValueupBackendApplication.class, args);
+    }
+
+}

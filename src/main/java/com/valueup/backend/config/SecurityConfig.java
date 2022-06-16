5a93997e0e96113296b142f491eddb76fc43ef9f
Koowin <56014482+Koowin@users.noreply.github.com>
2022-06-16 11:04:15 +0900

    feat: init security config

---
+++ src/main/java/com/valueup/backend/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.valueup.backend.config;
+
+import com.valueup.backend.oauth.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/login", "/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf(c -> c
+                        .ignoringAntMatchers("/h2-console/**")
+                )
+                .headers(h -> h
+                        .frameOptions().sameOrigin()
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/login").permitAll()
+                )
+                .oauth2Login(o -> o
+                        .loginPage("/login")
+                        .userInfoEndpoint().userService(customOAuth2UserService));
+        return http.build();
+    }
+}
---
+++ src/main/java/com/valueup/backend/oauth/OAuthAttributes.java
@@ -0,0 +1,4 @@
+package com.valueup.backend.oauth;
+
+public class OAuthAttributes {
+}

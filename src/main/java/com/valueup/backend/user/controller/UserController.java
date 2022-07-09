9bbef2ab951d6c493e2c04bef5d731e70455763b
Koowin <56014482+Koowin@users.noreply.github.com>
2022-07-09 22:38:03 +0900

    feat: enterprise AUTHORITY

--- src/main/java/com/valueup/backend/user/controller/UserController.java
+++ src/main/java/com/valueup/backend/user/controller/UserController.java
@@ -9,10 +9,7 @@ import com.valueup.backend.user.repository.UserRepository;
 import java.util.Optional;
 import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,8 +46,17 @@ public class UserController {
     return user;
   }
 
+  @GetMapping("/enterprise")
+  public User toEnterpriseRoleBySession(HttpSession session) {
+    User user = (User) session.getAttribute("user");
+    user.setRole(Role.ENTERPRISE);
+    userRepository.save(user);
+
+    return user;
+  }
+
   @PostMapping("/enterprise")
-  public User toEnterpriseRole(EnterpriseRequestDto requestDto) {
+  public User toEnterpriseRoleByNickname(EnterpriseRequestDto requestDto) {
     Optional<User> userOptional = userRepository.findByNickname(requestDto.getNickname());
     if (userOptional.isEmpty()) {
       //TODO Exception
